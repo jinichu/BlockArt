d6fabfd9620b0a02b34e36283d7a60b1e54f0196
jopika <jojo12345678@gmail.com>
2018-02-10 21:18:49 -0800

    Add a double fault key loading when creating inkMiner

--- inkminer/mining_test.go
+++ inkminer/mining_test.go
@@ -59,13 +59,6 @@ func TestInkMiner_CalculateState(t *testing.T) {
 	if err != nil {
 		t.Fatal("Unable to create inkMiner due to: ", err)
 	}
-	/*
-		PrevBlock string          // Hash of the previous block
-		BlockNum  int             // Block number
-		Records   []Operation     // Set of operation records
-		PubKey    ecdsa.PublicKey // Public key of the InkMiner that mined this block
-		Nonce     uint32
-	*/
 
 	// Generate Block 1
 	block1 := blockartlib.Block{}
@@ -165,67 +158,49 @@ func TestInkMiner_CalculateState(t *testing.T) {
 }
 
 func generateTestInkMiner() (*InkMiner, error) {
-	/*
-		addr      string							// IP Address of the InkMiner
-		client    *rpc.Client       				// RPC client to connect to the server
-		privKey   *ecdsa.PrivateKey 				// Pub/priv key pair of this InkMiner
-		publicKey string							// Public key of the Miner (Note: is this needed?)
-
-		latest        []*blockartlib.Block         	// Latest blocks in the blockchain
-		settings      server.MinerNetSettings 		// Settings for this BlockArt network instance
-		currentHead   blockartlib.Block       		// Block that InkMiner is mining on (current head)
-		mineBlockChan chan blockartlib.Block 		// Channel used to distribute blocks
-		rs            *rpc.Server 					// RPC Server
-		states        map[string]State 				// States of the canvas at a given block
-		stopper       *stopper.Stopper
-		log           *log.Logger
-
-		mu struct {
-			sync.Mutex
-
-			l               net.Listener
-			currentWIPBlock blockartlib.Block
-			peers           map[string]*peer
-
-			// blockchain is a map between blockhash and the block
-			blockchain map[string]blockartlib.Block
-			// all operations that haven't been added to the current block chain
-			mempool map[string]blockartlib.Operation
-		}
-	*/
-
 	rNum := rand.Int()%5 + 1
 	var basePath string = "../testkeys/test" + strconv.FormatInt(int64(rNum), 10)
 
+	// Attempt to load the privatekey from file
 	privKey, err := crypto.LoadPrivate(basePath+"-public.key", basePath+"-private.key")
 	if err != nil {
-		fmt.Println(err)
-		return nil, err
+		// If we fail the first time, try again with local path
+		err = nil
+		var basePath string = "./testkeys/test" + strconv.FormatInt(int64(rNum), 10)
+
+		privKey, err := crypto.LoadPrivate(basePath+"-public.key", basePath+"-private.key")
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		inkMiner, err := New(privKey)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		inkMiner.settings = server.MinerNetSettings{}
+		inkMiner.settings.GenesisBlockHash = "genesis!"
+		inkMiner.settings.InkPerOpBlock = 5
+		inkMiner.settings.InkPerNoOpBlock = 10
+		inkMiner.settings.PoWDifficultyNoOpBlock = 1
+		inkMiner.settings.PoWDifficultyOpBlock = 2
+		return inkMiner, nil
+	} else {
+		inkMiner, err := New(privKey)
+		if err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+
+		inkMiner.settings = server.MinerNetSettings{}
+		inkMiner.settings.GenesisBlockHash = "genesis!"
+		inkMiner.settings.InkPerOpBlock = 5
+		inkMiner.settings.InkPerNoOpBlock = 10
+		inkMiner.settings.PoWDifficultyNoOpBlock = 1
+		inkMiner.settings.PoWDifficultyOpBlock = 2
+
+		return inkMiner, nil
 	}
-
-	//publicKey, err := crypto.UnmarshalPublic("-----BEGIN EC PRIVATE KEY-----MHcCAQEEILPTz2axlGpLMPNsRZbjViv0tTfbYrtHHYDE0MXL3wQzoAoGCCqGSM49AwEHoUQDQgAE8d5kb89KLirtIuEEcfOIuUSIlVy2KkgGBc5CrZXaIxwHl17sfQxDx8Ps5M1hsBm2GzeHdUbUnn5M4iX1sg15yg==-----END EC PRIVATE KEY-----")
-	//if err != nil {
-	//	return nil, err
-	//}
-	//privateKey, err := crypto.UnmarshalPrivate("-----BEGIN PUBLIC KEY-----MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE8d5kb89KLirtIuEEcfOIuUSIlVy2KkgGBc5CrZXaIxwHl17sfQxDx8Ps5M1hsBm2GzeHdUbUnn5M4iX1sg15yg==-----END PUBLIC KEY-----")
-	//if err != nil {
-	//	return nil, err
-	//}
-
-	//privateKey.PublicKey = *publicKey
-
-	inkMiner, err := New(privKey)
-	if err != nil {
-		fmt.Println(err)
-		return nil, err
-	}
-
-	inkMiner.settings = server.MinerNetSettings{}
-	inkMiner.settings.GenesisBlockHash = "genesis!"
-	inkMiner.settings.InkPerOpBlock = 5
-	inkMiner.settings.InkPerNoOpBlock = 10
-	inkMiner.settings.PoWDifficultyNoOpBlock = 1
-	inkMiner.settings.PoWDifficultyOpBlock = 2
-
-	return inkMiner, nil
 }
